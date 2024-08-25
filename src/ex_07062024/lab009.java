168751284fa26563a54f80ee1435cb681ab9d5b0
a2z <dibyas83@gmail.com>
2024-08-25 09:47:03 +0530

    2nd day

--- src/ex_07062024/lab009.java
+++ src/ex_07062024/lab009.java
@@ -2,6 +2,18 @@ package ex_07062024;
 
 public class lab009 {
     public static void main(String[] args) {
-        System.out.println();
+        int val =300;
+        long ph_no=9844223433L;
+        float pi=3.14F;
+        float x=val+pi;
+        byte m=10;
+        byte bi = (byte)val;
+        // invalid explicit casting narrowingw
+        // type casting 44 will comeout
+        int n = (int)m;
+        System.out.println(x);
+        System.out.println(bi);
+        System.out.println(n);
+        // src and dst conv
     }
 }
---
+++ src/ex_07062024/lab010.java
@@ -0,0 +1,19 @@
+package ex_07062024;
+
+public class lab010 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a; // a=a+1
+        // exp=11,a=11
+        int c=12;
+        int a_post=15;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c++);
+        System.out.println(c);
+        System.out.println(++c);
+        System.out.println(a_post++);
+        System.out.println(a_post);
+
+    }
+}
