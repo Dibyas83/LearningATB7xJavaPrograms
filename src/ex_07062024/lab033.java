4ccf5db8442c37f4786c09113bf60e045945e6c0
a2z <dibyas83@gmail.com>
2024-09-13 21:55:45 +0530

    2nd day

---
+++ src/ex_07062024/lab033.java
@@ -0,0 +1,13 @@
+package ex_07062024;
+
+public class lab033 {
+    public static void main(String[] args) {
+        //dataTypes[] arrayName;
+        //all should be int or string.char
+        int[] marks_board ={90.40.60.70};
+        String shopping_list[] ={"bread"."milk"};
+        char[] grades= {'a','b'};
+        float[] ad={14.3f,12.5f};
+        
+    }
+}
