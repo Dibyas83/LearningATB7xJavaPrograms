c9d9bffb4ce819f2ef716b3342c793e064580416
a2z <dibyas83@gmail.com>
2024-09-19 18:02:25 +0530

    2nd day

--- src/ex_07062024/aug24/lab1.java
+++ src/ex_07062024/aug24/lab1.java
@@ -1,4 +1,28 @@
 package ex_07062024.aug24;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class lab1 {
+    public static void main(String[] args) {
+        //arraylist elements changes places in memory if new element comes in middle or deleted
+        //linked list stores the next and previous references consumes more memory ?(doubly linked),not shifting so easy for manipulation (insert,delete) slow in accessing the elements
+
+        //allow duplicates,variable size(load factor)and type in linklst
+        //vector array list with thread safety
+        LinkedList er=new LinkedList();
+        er.add("node 1");
+        er.add("node 2");
+        er.add("node 2");
+        er.add("node 4");
+        System.out.println(er);
+
+        ArrayList err=new ArrayList();
+        err.add("node 1");
+        err.add("node 2");
+        err.add("node 2");
+        err.add("node 4");
+        System.out.println(err);
+
+    }
 }
--- src/ex_07062024/aug24/lab2.java
+++ src/ex_07062024/aug24/lab2.java
@@ -1,4 +1,24 @@
 package ex_07062024.aug24;
 
+import java.util.Stack;
+
 public class lab2 {
+    public static void main(String[] args) {
+        //vector,Stack-legacy class
+        Stack s=new Stack();
+        s.add("pp");
+        s.add("ppl");
+        s.add("pj");
+        s.add("pk");
+        s.push("ph");
+        s.push("pf");
+
+
+        System.out.println(s);//it is LIFO,removes pk first
+        //it peek(looking or searching)_pop(remove),push(insert),search as added function +vectoor functions
+        //it is heterogenous
+        System.out.println(s.peek());//last one
+        System.out.println(s.pop());//used in library,cannot remove from middle
+
+    }
 }
